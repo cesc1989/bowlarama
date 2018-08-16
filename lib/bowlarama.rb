053608837e49f8eff97a6005ca9564fea7523a66
Francisco Quintero Linux Mint Thinkpad <frajaquico@yahoo.es>
2018-08-15 22:45:05 -0500

    Collect players and scores

--- lib/bowlarama.rb
+++ lib/bowlarama.rb
@@ -1,5 +1,20 @@
 require "bowlarama/version"
 
 module Bowlarama
-  # Your code goes here...
+  SCORES_FILE = "#{Dir.pwd}/spec/fixtures/scores.txt".freeze
+
+  game = {}
+
+  File.foreach(SCORES_FILE) do |l|
+    splitted = l.split(' ')
+    player_key = splitted.first.to_sym
+
+    if game.key?(player_key)
+      game[player_key] << splitted[1]
+    else
+      game[player_key] = [splitted[1]]
+    end
+  end
+
+  puts game
 end
