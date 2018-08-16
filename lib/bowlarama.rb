f61f117928bf764beeb4a69bca7f46f6012cb537
Francisco Quintero Linux Mint Thinkpad <frajaquico@yahoo.es>
2018-08-16 18:24:03 -0500

    Create instances of Game::Player to calculate order pinfalls

--- lib/bowlarama.rb
+++ lib/bowlarama.rb
@@ -1,20 +1,25 @@
 require "bowlarama/version"
+require 'bowlarama/game'
 
 module Bowlarama
   SCORES_FILE = "#{Dir.pwd}/spec/fixtures/scores.txt".freeze
 
-  game = {}
+  match_sheet = {}
 
   File.foreach(SCORES_FILE) do |l|
     splitted = l.split(' ')
     player_key = splitted.first.to_sym
 
-    if game.key?(player_key)
-      game[player_key] << splitted[1]
+    if match_sheet.key?(player_key)
+      match_sheet[player_key] << splitted.last
     else
-      game[player_key] = [splitted[1]]
+      match_sheet[player_key] = [splitted.last]
     end
   end
 
-  puts game
+  player_1 = Game::Player.new(name: match_sheet.keys.first, pinfalls: match_sheet.values.first)
+  player_2 = Game::Player.new(name: match_sheet.keys.last, pinfalls: match_sheet.values.last)
+
+  player_1.scores_per_frame
+  player_2.scores_per_frame
 end
