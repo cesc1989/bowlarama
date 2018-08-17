b93b8d79d796411ac28254293a29dce9207d3b1f
Francisco Quintero Linux Mint Thinkpad <frajaquico@yahoo.es>
2018-08-16 20:48:31 -0500

    Define function for module Bowlarama

--- lib/bowlarama.rb
+++ lib/bowlarama.rb
@@ -2,24 +2,26 @@ require "bowlarama/version"
 require 'bowlarama/game'
 
 module Bowlarama
-  SCORES_FILE = "#{Dir.pwd}/spec/fixtures/scores.txt".freeze
+  def self.score_sheet(scores_file)
+    match_sheet = {}
 
-  match_sheet = {}
+    File.foreach(scores_file) do |l|
+      splitted = l.split(' ')
+      player_key = splitted.first.to_sym
 
-  File.foreach(SCORES_FILE) do |l|
-    splitted = l.split(' ')
-    player_key = splitted.first.to_sym
-
-    if match_sheet.key?(player_key)
-      match_sheet[player_key] << splitted.last
-    else
-      match_sheet[player_key] = [splitted.last]
+      if match_sheet.key?(player_key)
+        match_sheet[player_key] << splitted.last
+      else
+        match_sheet[player_key] = [splitted.last]
+      end
     end
+
+    player_1 = Game::Player.new(name: match_sheet.keys.first, pinfalls: match_sheet.values.first)
+    player_2 = Game::Player.new(name: match_sheet.keys.last, pinfalls: match_sheet.values.last)
+
+    player_1.assign_rolls_to_frames
   end
-
-  player_1 = Game::Player.new(name: match_sheet.keys.first, pinfalls: match_sheet.values.first)
-  player_2 = Game::Player.new(name: match_sheet.keys.last, pinfalls: match_sheet.values.last)
-
-  player_1.scores_per_frame
-  player_2.scores_per_frame
 end
+
+# Bowlarama.score_sheet("#{Dir.pwd}/spec/fixtures/scores.txt")
+Bowlarama.score_sheet("#{Dir.pwd}/spec/fixtures/scores_sin_bonos.txt")
